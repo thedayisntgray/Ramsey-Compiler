37d0fcf8d176c123c65a91310e69accc00da14a4
Landon Gray <wlg11a@acu.edu>
2014-10-29 07:47:57 -0500

    Fix file indentation and remove white space

--- ramsey/scan.cpp
+++ ramsey/scan.cpp
@@ -7,276 +7,226 @@
 #include "tokenizer.h"
 
 int main (int argc, char *argv[]) {
-  //char str[256];
 
-  //std::cout << "Enter the name of an existing text file: ";
-  // std::cout << "name of program is " << argv[0] << "\n";
-  std::string source = argv[1];
-  // std::cin >> source;
+	//allows compiled program to accept a source file via command line
+	std::string source = argv[1];
 
-  std::string filename = preprocessor(source);
+	std::string filename = preprocessor(source);
 
-  std::ifstream is(filename);     // open file
+	std::ifstream is(filename);
 
-  std::string lex = "";
-  int lineNum = 1;
+	std::string lex = "";
+	int lineNum = 1;
 
-  token t;
-  std::queue<token> qlist;
+	token t;
+	std::queue<token> qlist;
 
-  while (is.good())
-  {
+	while (is.good())
+	{
+		char c = is.get();
+		std::string cn = charToStr(c);
 
-    char c = is.get();
-    std::string cn = charToStr(c);
+		char p = is.peek();
+		std::string peek = charToStr(p);
 
-    char p = is.peek();
-    std::string peek = charToStr(p);
+		while(is.good()){
 
-    // if(c == '\n' || p == '\n' || getTokenType(cn) == "EOL" || getTokenType(peek) == "EOL" )
-    //   lineNum++;
+			if(c == '\n'  ||  getTokenType(cn) == "EOL" || getTokenType(peek) == "EOL" ){
+				lineNum++;
+				t.token = "EOL";
+				t.lexeme = '\n';
+				t.lineNumber = lineNum;
+				lex = "";
+				qlist.push(t);
+				break;
+			}
+			if(cn == "" || cn == " ")
+				break;
 
-    while(is.good()){
+			lex += cn;
+			//single parens
+			if(lex == "("){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if( peek == "(" || peek == ")" || peek == "+" || peek == "*" || peek == "/" || peek == "," || ((getTokenType(lex) == "IDENT" || getTokenType(lex) == "PLUS" || getTokenType(lex) == "MINUS" || getTokenType(lex) == "DIV" || getTokenType(lex) == "MUL" || getTokenType(lex) == "RPAREN") && peek == "-")){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == ")"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == ","){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			//single operators
+			if(lex == "="){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "+"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			//case for negative numbers
+			if(lex == "-" && getTokenType(peek) == "LITERAL"){
+				c = is.get();
+				cn = charToStr(c); 
+				while(getTokenType(cn) == "LITERAL"){
+					lex += cn;
+					c = is.get();
+					cn = charToStr(c);
+				}
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
 
-    if(c == '\n'  ||  getTokenType(cn) == "EOL" || getTokenType(peek) == "EOL" ){
-      lineNum++;
-        t.token = "EOL";
-        t.lexeme = '\n';
-        t.lineNumber = lineNum;
-        lex = "";
-        qlist.push(t);
-        break;
+				p = is.peek();
+				peek = charToStr(p);
+			}
+			//TODO These two if cases should be combined
+			if(lex == "-"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "/"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "*"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			//double operators
+			if(lex == "<" && peek == "-"){//lex == "<" && peek == "-"){
+				c = is.get(); // c = "-"
+				lex += c; // lex = "<-"
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "<" && (getTokenType(peek) == "IDENT"  || getTokenType(peek) == "LITERAL" || peek == " ") ){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == ">" && (getTokenType(peek) == "IDENT"  || getTokenType(peek) == "LITERAL" || peek == " ") ){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "<" && peek == "="){
+				c = is.get();
+				lex += c;
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == ">" && peek == "="){
+				c = is.get();
+				lex += c;
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "!" && peek == "="){
+				c = is.get();
+				lex += c;
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(lex == "!" && peek == "="){
+				c = is.get();
+				lex += c;
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if( lex == "\n"){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lineNum++;
+				lex = "";
+				break;
+			}
+			if( (lex == "while" && peek == "") || (lex == "endwhile" && peek == "") || (lex == "fun" && (peek == "(" || peek == "")) || (lex == "endfun" && peek == "") || (lex == "as" && peek == "") || (lex == "and" && peek == "") || (lex == "or" && peek == "") || (lex == "toss" && peek == "") || (lex == "take" && peek == "") || (lex == "not" && (peek == "(" || peek == "")) || (lex == "big" && peek == "") || (lex == "small" && peek == "") || (lex == "boo" && peek == "") || (lex == "mod" && peek == "") || (lex == "if" && (peek == "" || peek == "(")) || (lex == "elf" && (peek == "" || peek == "(")) || (lex == "else" && peek == "") || (lex == "endif" && peek == "") || (lex == "give" && peek == "") || (lex == "chop" && peek == "") ){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if(peek == ""){
+				t.token = getTokenType(lex);
+				t.lexeme = lex;
+				t.lineNumber = lineNum;
+				lex = "";
+				break;
+			}
+			if( getTokenType(lex) == "INVALID"){
+				std::cout << "invalid tokent:" << lex << "\n";
+				break;
+			}
+			c = is.get();
+			cn = charToStr(c);
 
-    }
+			p = is.peek();
+			peek = charToStr(p);
+			}
+			if (cn != "" and cn != " ")
+				qlist.push(t);
+		}
 
-      if(cn == "" || cn == " ")
-        break;
-
-      lex += cn;
-      //single parens
-      if(lex == "("){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if( peek == "(" ||
-          peek == ")" ||
-          peek == "+" ||
-          peek == "*" ||
-          peek == "/" ||
-          peek == "," ||
-
-          ((getTokenType(lex) == "IDENT" ||
-            getTokenType(lex) == "PLUS" ||
-            getTokenType(lex) == "MINUS" ||
-            getTokenType(lex) == "DIV" ||
-            getTokenType(lex) == "MUL" ||
-            getTokenType(lex) == "RPAREN")
-            && peek == "-")
-          ){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == ")"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == ","){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      //single operators
-      if(lex == "="){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "+"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      //case for negative numbers
-      if(lex == "-" && getTokenType(peek) == "LITERAL"){
-        c = is.get();
-        cn = charToStr(c); 
-
-        while(getTokenType(cn) == "LITERAL"){
-          lex += cn;
-          c = is.get();
-          cn = charToStr(c);
-
-        }
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-
-        p = is.peek();
-        peek = charToStr(p);
-      }
-      //TODO These two if cases should be combined
-      if(lex == "-"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "/"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "*"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      //double operators
-      if(lex == "<" && peek == "-"){//lex == "<" && peek == "-"){
-        c = is.get(); // c = "-"
-        lex += c; // lex = "<-"
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "<" && (getTokenType(peek) == "IDENT"  || getTokenType(peek) == "LITERAL" || peek == " ") ){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == ">" && (getTokenType(peek) == "IDENT"  || getTokenType(peek) == "LITERAL" || peek == " ") ){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "<" && peek == "="){
-        c = is.get();
-        lex += c;
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == ">" && peek == "="){
-        c = is.get();
-        lex += c;
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "!" && peek == "="){
-        c = is.get();
-        lex += c;
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(lex == "!" && peek == "="){
-        c = is.get();
-        lex += c;
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if( lex == "\n"){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lineNum++;
-        lex = "";
-        break;
-      }
-      if( (lex == "while" && peek == "") ||
-          (lex == "endwhile" && peek == "") ||
-          (lex == "fun" && (peek == "(" || peek == "")) ||
-          (lex == "endfun" && peek == "") ||
-          (lex == "as" && peek == "") ||
-          (lex == "and" && peek == "") ||
-          (lex == "or" && peek == "") ||
-          (lex == "toss" && peek == "") ||
-          (lex == "take" && peek == "") ||
-          (lex == "not" && (peek == "(" || peek == "")) ||
-	        (lex == "big" && peek == "") ||
-	        (lex == "small" && peek == "") ||
-          (lex == "boo" && peek == "") ||
-          (lex == "mod" && peek == "") ||
-          (lex == "if" && (peek == "" || peek == "(")) ||
-          (lex == "elf" && (peek == "" || peek == "(")) ||
-          (lex == "else" && peek == "") ||
-          (lex == "endif" && peek == "") ||
-          (lex == "give" && peek == "") ||
-          (lex == "chop" && peek == "") ){
-
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if(peek == ""){
-        t.token = getTokenType(lex);
-        t.lexeme = lex;
-        t.lineNumber = lineNum;
-        lex = "";
-        break;
-      }
-      if( getTokenType(lex) == "INVALID"){
-        std::cout << "invalid tokent:" << lex << "\n";
-        break;
-      }
-
-      c = is.get();
-      cn = charToStr(c);
-
-      p = is.peek();
-      peek = charToStr(p);
-    }
-    if (cn != "" and cn != " ")
-      qlist.push(t);
-  }
-
-  is.close();
-  while (!qlist.empty())
-  {
-    std::cout << qlist.front().token << "\n";
-
-    // std::cout << "Lexeme: " << qlist.front().lexeme << "\n" << "Token: " << qlist.front().token << "\n";
-    // std::cout << "Line Number: " << qlist.front().lineNumber << "\n";
-    qlist.pop();
-  }
-}
+		is.close();
+		while (!qlist.empty()){
+			std::cout << qlist.front().token << "\n";
+			qlist.pop();
+		}
+	}
--- ramsey/tokenizer.h
+++ ramsey/tokenizer.h
@@ -3,60 +3,60 @@
 #include <regex>
 
 std::string getTokenType(std::string lexeme){
-    //LITERAL
-    if (std::regex_match (lexeme, std::regex("-?[0-9]+") ))
-        return "LITERAL";
-    //END OF LINE
-    if (std::regex_match (lexeme, std::regex("\n") ))
-        return "EOL";
-    //ASSIGN
-    if (std::regex_match (lexeme, std::regex("^<-$") ))
-        return "ASSIGN";
-    if (std::regex_match (lexeme, std::regex(">=|<=|!=|=|<|>") ))
-        return "COMPOP";
-    if (std::regex_match (lexeme, std::regex("\\+|-|/|\\*|mod") ))
-	return "NUMOP";
-    if (std::regex_match (lexeme, std::regex("^,$") ))
-        return "COMMA";
-    if (std::regex_match (lexeme, std::regex("^if$") ))
-        return "IF";
-    if (std::regex_match (lexeme, std::regex("^elf$") ))
-        return "ELF";
-    if (std::regex_match (lexeme, std::regex("^else$") ))
-        return "ELSE";
-    if (std::regex_match (lexeme, std::regex("^endif$") ))
-        return "ENDIF";
-    if (std::regex_match (lexeme, std::regex("^while$") ))
-        return "WHILE";
-    if (std::regex_match (lexeme, std::regex("^endwhile$") ))
-        return "ENDWHILE";
-    if (std::regex_match (lexeme, std::regex("^take$") ))
-        return "TAKE";
-    if (std::regex_match (lexeme, std::regex("^toss$") ))
-        return "TOSS";
-    if (std::regex_match (lexeme, std::regex("^fun$") ))
-        return "FUN";
-    if (std::regex_match (lexeme, std::regex("^endfun$") ))
-        return "ENDFUN";
-    if (std::regex_match (lexeme, std::regex("^as$") ))
-        return "AS";
-    if (std::regex_match (lexeme, std::regex("^and$") ))
-        return "AND";
-    if (std::regex_match (lexeme, std::regex("^or$") ))
-        return "OR";
-    if (std::regex_match (lexeme, std::regex("^give$") ))
-        return "GIVE";
-    if (std::regex_match (lexeme, std::regex("^\\($") ))
-        return "LPAREN";
-    if (std::regex_match (lexeme, std::regex("^\\)$") ))
-        return "RPAREN";
-    if (std::regex_match (lexeme, std::regex("boo|in|big|small") ))
-	return "TYPE";
-    if (std::regex_match (lexeme, std::regex("^chop$") ))
-        return "CHOP";
-        //IDENTIFIER
-    if (std::regex_match (lexeme, std::regex("[a-zA-Z]+[a-zA-Z0-9_]*") ))
-        return "IDENT";
+	//LITERAL
+	if (std::regex_match (lexeme, std::regex("-?[0-9]+") ))
+		return "LITERAL";
+	//END OF LINE
+	if (std::regex_match (lexeme, std::regex("\n") ))
+		return "EOL";
+	//ASSIGN
+	if (std::regex_match (lexeme, std::regex("^<-$") ))
+		return "ASSIGN";
+	if (std::regex_match (lexeme, std::regex(">=|<=|!=|=|<|>") ))
+		return "COMPOP";
+	if (std::regex_match (lexeme, std::regex("\\+|-|/|\\*|mod") ))
+		return "NUMOP";
+	if (std::regex_match (lexeme, std::regex("^,$") ))
+		return "COMMA";
+	if (std::regex_match (lexeme, std::regex("^if$") ))
+		return "IF";
+	if (std::regex_match (lexeme, std::regex("^elf$") ))
+		return "ELF";
+	if (std::regex_match (lexeme, std::regex("^else$") ))
+		return "ELSE";
+	if (std::regex_match (lexeme, std::regex("^endif$") ))
+		return "ENDIF";
+	if (std::regex_match (lexeme, std::regex("^while$") ))
+		return "WHILE";
+	if (std::regex_match (lexeme, std::regex("^endwhile$") ))
+		return "ENDWHILE";
+	if (std::regex_match (lexeme, std::regex("^take$") ))
+		return "TAKE";
+	if (std::regex_match (lexeme, std::regex("^toss$") ))
+		return "TOSS";
+	if (std::regex_match (lexeme, std::regex("^fun$") ))
+		return "FUN";
+	if (std::regex_match (lexeme, std::regex("^endfun$") ))
+		return "ENDFUN";
+	if (std::regex_match (lexeme, std::regex("^as$") ))
+		return "AS";
+	if (std::regex_match (lexeme, std::regex("^and$") ))
+		return "AND";
+	if (std::regex_match (lexeme, std::regex("^or$") ))
+		return "OR";
+	if (std::regex_match (lexeme, std::regex("^give$") ))
+		return "GIVE";
+	if (std::regex_match (lexeme, std::regex("^\\($") ))
+		return "LPAREN";
+	if (std::regex_match (lexeme, std::regex("^\\)$") ))
+		return "RPAREN";
+	if (std::regex_match (lexeme, std::regex("boo|in|big|small") ))
+		return "TYPE";
+	if (std::regex_match (lexeme, std::regex("^chop$") ))
+		return "CHOP";
+	//IDENTIFIER
+	if (std::regex_match (lexeme, std::regex("[a-zA-Z]+[a-zA-Z0-9_]*") ))
+		return "IDENT";
 
-    return "INVALID";
+	return "INVALID";
 }
